754b111fd97e9827628d65c40320333a13908430
Vinyzada <vinyescolas@gmail.com>
2024-03-22 17:12:13 -0300

    Linkado front-end ao back-end

--- src/api/categorias.js
+++ src/api/categorias.js
@@ -1,19 +1,29 @@
 import axios from "axios";
 export default class CategoriasApi {
+  constructor() {
+    this.baseUrl = "http://127.0.0.1:8000";
+  }
+
   async buscarTodasAsCategorias() {
-    const { data } = await axios.get("/categorias/");
+    const { data } = await axios.get(`${this.baseUrl}/categorias/`);
     return data.results;
   }
   async adicionarCategoria(categoria) {
-    const { data } = await axios.post("/categorias/", categoria);
+    const { data } = await axios.post(
+      `${this.baseUrl}/categorias/`,
+      this.baseUrlcategoria
+    );
     return data.results;
   }
   async atualizarCategoria(categoria) {
-    const { data } = await axios.put(`/categorias/${categoria.id}/`, categoria);
+    const { data } = await axios.put(
+      `${this.baseUrl}/categorias/${categoria.id}/`,
+      categoria
+    );
     return data.results;
   }
   async excluirCategoria(id) {
-    const { data } = await axios.delete(`/categorias/${id}/`);
+    const { data } = await axios.delete(`${this.baseUrl}/categorias/${id}/`);
     return data.results;
   }
 }
